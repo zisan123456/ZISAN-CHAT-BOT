c6eab79570869c34510f62ee88afbf3823041565
zisan123456 <dxgishan68@gmail.com>
2026-07-26 03:20:39 +0600

    Update bot messages with new branding

--- Script/events/antiout.js
+++ Script/events/antiout.js
@@ -17,11 +17,11 @@ module.exports.run = async({ event, api, Threads, Users }) => {
    if (error) {
     api.sendMessage(`সরি বস, ${name} কে আবার এড করতে পারলাম না। 
 সম্ভবত উনি বটকে ব্লক করেছে অথবা তার প্রাইভেসি সেটিংসের কারণে এড করা যায় না। 
-\n──────꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭─────`, event.threadID)
+\n──────꯭─⃝‌‌SK ZISAN BOT─────`, event.threadID)
    } else api.sendMessage(`শোন, ${name}, এই গ্রুপ হইলো গ্যাং!
 এখান থেকে যাইতে হলে এডমিনের পারমিশন লাগে!
 তুই পারমিশন ছাড়া লিভ নিছোস – তোকে আবার মাফিয়া স্টাইলে এড দিলাম।
-\n──────꯭─⃝‌‌𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭─────`, event.threadID);
+\n──────꯭─⃝‌‌SK ZISAN BOT─────`, event.threadID);
   })
  }
 }
