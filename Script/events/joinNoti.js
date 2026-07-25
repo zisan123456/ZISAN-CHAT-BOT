8bd37d3e08ba93aac8ec485e96d48c25879cc5c8
zisan123456 <dxgishan68@gmail.com>
2026-07-26 04:04:33 +0600

    Change bot name from Shahadat to Zisan Chat Bot

--- Script/events/joinNoti.js
+++ Script/events/joinNoti.js
@@ -28,7 +28,7 @@ module.exports.run = async function({ api, event }) {
   const { threadID } = event;
   
   const botPrefix = global.config.PREFIX || "/";
-  const botName = global.config.BOTNAME || "𝗦𝗵𝗮𝗵𝗮𝗱𝗮𝘁 𝗖𝗵𝗮𝘁 𝗕𝗼𝘁";
+  const botName = global.config.BOTNAME || "ZISAN CHAT BOT";
 
  
   if (event.logMessageData.addedParticipants.some(i => i.userFbId == api.getCurrentUserID())) {
